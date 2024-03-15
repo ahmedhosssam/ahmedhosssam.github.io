04a26cbdb2e4da98adecd5849c08baa10303de38
Ahmed Hossam <hoss4614@gmail.com>
2024-03-15 09:58:01 +0200

    remove everything

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Components
-import Header from './Components/Header/Header';
-import Summary from './Components/Summary/Summary';
-import Skills from './Components/Skills/Skills';
-import Projects from './Components/Projects/Projects';
-import ContactMe from './Components/ContatctMe/ContactMe';
-
-// Styles
-import './Styles/App.scss';
-import './Styles/header.scss';
-import './Styles/variables.scss';
-import './Styles/Summary.scss';
-import './Styles/skills.scss';
-import './Styles/projects.scss';
-import './Styles/projectitem.scss';
-import './Styles/contactme.scss';
+import App from  './Components/App/App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <Summary />
-    <Skills />
-    <Projects />
-    <ContactMe />
+    <App/>
   </React.StrictMode>
 );
