1c42d022c60a2e22f51b2a530235e0c6e9307091
Ahmed Hossam <hoss4614@gmail.com>
2024-03-15 10:25:13 +0200

    remove styles plus add header

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,6 +1,16 @@
+import Header from '../Header/Header.jsx'
+import Posts from '../Posts/Posts.jsx'
+
 const App = () => {
+    const st = {
+        'fontFamily': 'Lucida Console, monospace'
+    }
     return (
-        <div>hi</div>
+        <div style={st}>
+            <Header/>
+            <hr/>
+            <Posts/>
+        </div>
     )
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Ahmed Hossam</h1>
+            <div>Hi, I'm Ahmed Hossam, a programmer, computer science student, and aspiring engineer.</div>
+            Links: <a href="https://github.com/ahmedhosssam">github</a> - <a href="https://twitter.com/ahmedhossvm">twitter(X)</a> - <a href="https://linkedin.com/in/ahmedhossamm">linkedin</a>
+
+        </div>
+    )
+}
+
+export default Header
