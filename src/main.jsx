444800877154fbd0212a8ea04d87ae6d5079ac46
Ahmed Hossam <hoss4614@gmail.com>
2024-03-15 13:27:26 +0200

    try creatHashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {createHashRouter, RouterProvider} from 'react-router-dom';
 
 import App from  './Components/App/App.jsx'
 import Header from  './Components/Header/Header.jsx'
 
 import Test from  './Components/Posts/Posts/Test.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App/>
